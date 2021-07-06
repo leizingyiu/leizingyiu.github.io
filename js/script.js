98a6419e29ce0b3ae7b2a3506ad633f957bf5c6b
leizingyiu <youcanfindme@126.com>
2021-07-07 01:46:56 +0800

    js of li part is  done

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var debugBoo = false;
+console.log = debugBoo ? console.log : function () { return };
+
+var activeClassName = 'active';
+[...document.querySelectorAll('#main li')].map(function (li) {
+    li.addEventListener('click', function clickLi() {
+
+        /* TODO */
+        var p = li.parentNode;
+        switch (true) {
+            case (li.querySelector('ul,ol') != null && li.className.indexOf(activeClassName) == -1 && li.hasAttribute('data-text')):
+                /* 点击方块，方块切换成active */
+
+                while (p.className.indexOf(activeClassName) == -1) {
+                    p = p.parentNode;
+                    if (p.localName == 'body') { return; }
+                }
+                p.classList.remove(activeClassName);
+                this.classList.add(activeClassName);
+                break;
+
+            case (li.hasAttribute('data-title') && li.clientHeight != 0):
+                /* 点击标题右侧返回按钮，返回上一级 */
+
+                if (li.className.indexOf(activeClassName) == -1) {
+                    while (p.className.indexOf(activeClassName) == -1) {
+                        p = p.parentNode;
+                    }
+                    p.classList.remove(activeClassName);
+                }
+                do {
+                    p = p.parentNode;
+                } while (p.localName != 'li' && !p.hasAttribute('data-title'))
+
+                p.classList.add(activeClassName);
+                this.classList.remove(activeClassName);
+
+                break;
+            case ([...li.children].map(c => c.localName).indexOf('a') != -1 && li.className.indexOf(activeClassName) == -1):
+                /* 点击方块，方块点击内部超链接 */
+
+                li.children[[...li.children].map(c => c.localName).indexOf('a')].click();
+                break;
+
+        }
+        window.event.stopPropagation();
+        return false;
+    }, false)
+})
+
+
+console.log('loaded script.js')
