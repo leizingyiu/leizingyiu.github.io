4b415d837b24fc776d7782c3774f25edae1b9dc0
leizingyiu <youcanfindme@126.com>
2021-07-15 03:08:28 +0800

    temp

--- js/writeDataPreview.js
+++ js/writeDataPreview.js
@@ -3,8 +3,6 @@
     if (a.hasAttribute('style') && a.style.cssText.indexOf("--bg-image") == -1) {
         a.style = a.style + ';' + bgImage;
     } else if (a.hasAttribute('style') && a.style.cssText.indexOf('--bg-image') != -1) {
-        console.log(a.style.cssText);
-        console.log(a.style.cssText.match(/(--bg-image:\surl\(['"])[^'"]*(['"]\);)/g));
 
         a.style = a.style.cssText.replace(/(--bg-image:\surl\(['"])[^'"]*(['"]\);)/g, '$1' + a.getAttribute('data-preview') + '$2');
     } else {
