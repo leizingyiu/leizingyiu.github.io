0b765fee0b76c8e50dcc5f85c221ebb44ac6434d
leizingyiu <youcanfindme@126.com>
2021-07-26 16:46:52 +0800

    完善blogRssReader

---
+++ js/blogRssReader.js
@@ -0,0 +1,45 @@
+
+iframe = document.createElement('iframe');
+iframe.src = window.location.origin.match('leizingyiu.net') ? location.protocol + '//blog.leizingyiu.net/rssPage/index.html' : 'http://localhost:4000/rssPage/index.html';
+iframe.id = 'rssReader';
+iframe.style.cssText = 'opacity:0;overflow:hidden;';
+iframe.frameborder = '0';
+iframe.width = iframe.height = 0;
+
+iframe.onload = function () {
+    document.getElementById(iframe.id).contentWindow.postMessage('LatestBlog:8', iframe.src.match(/.*:[\/]{2}[^\/]+/)[0]);
+}
+
+document.getElementsByTagName('body')[0].appendChild(iframe);
+
+function write_ul_li_a(contents, containerSelector) {
+    var ul = document.createElement('ul');
+    if (!document.querySelector(containerSelector)) { return; }
+    document.querySelector(containerSelector).innerHTML = '';
+    document.querySelector(containerSelector).appendChild(ul);
+    contents.map(function (c) {
+        li = document.createElement('li');
+        a = document.createElement('a');
+        a.href = c.href;
+        a.innerText = c.title;
+        ul.appendChild(li);
+        li.appendChild(a);
+    })
+}
+
+write_ul_li_a([{ 'href': '', 'title': ' 数 据 加 载 中 ， 请 稍 后 ' }], '#blog');
+window.addEventListener('message', function (e) {
+    var messageObj = {};
+    try {
+        messageObj = JSON.parse(e.data);
+    } catch (err) {
+        console.log(err);
+        return;
+    }
+    Object.keys(messageObj).map(function (k) {
+        write_ul_li_a(messageObj[k], '#' + k);
+        liSetup();
+        aSetup();
+    });
+
+}, false);
--- js/script.js
+++ js/script.js
@@ -2,86 +2,86 @@ var debugBoo = false;
 debugBoo = window.location.hostname.match(/[a-zA-Z]/) == null ? true : debugBoo;
 console.log = debugBoo ? console.log : function () { return };
 
+function liSetup(selector = '#main li') {
+    var activeClassName = 'active';
+    var liSetupMark = 'setuped';
 
+    [...document.querySelectorAll(selector)].map(function (li) {
 
+        if (li.hasAttribute(liSetupMark)) { return; }
+        li.setAttribute(liSetupMark, '');
+        li.addEventListener('click', function clickLi() {
+            console.log(this);
+            console.log('\n\n clickLi');
+            console.log(li);
 
-var activeClassName = 'active';
-[...document.querySelectorAll('#main li')].map(function (li) {
-    li.addEventListener('click', function clickLi() {
+            var p = li.parentNode;
+            switch (true) {
+                case (li.querySelector('ul,ol') != null && li.className.indexOf(activeClassName) == -1 && li.hasAttribute('data-openInside')):
+                    /* 点击方块，方块切换成active */
 
-
-        var p = li.parentNode;
-        switch (true) {
-            case (li.querySelector('ul,ol') != null && li.className.indexOf(activeClassName) == -1 && li.hasAttribute('data-openInside')):
-                /* 点击方块，方块切换成active */
-
-                while (p.className.indexOf(activeClassName) == -1) {
-                    p = p.parentNode;
-                    if (p.localName == 'body') { return; }
-                }
-                p.classList.remove(activeClassName);
-                this.classList.add(activeClassName);
-                break;
-
-            case (li.hasAttribute('data-title') && li.clientHeight != 0):
-                /* 点击标题右侧返回按钮，返回上一级 */
-                /* TODO :参考 a 点击 */
-
-                if (li.className.indexOf(activeClassName) == -1) {
+                    console.log("li.querySelector('ul,ol') != null && li.className.indexOf(activeClassName) == -1 && li.hasAttribute('data-openInside') : " + (li.querySelector('ul,ol') != null && li.className.indexOf(activeClassName) == -1 && li.hasAttribute('data-openInside')));
                     while (p.className.indexOf(activeClassName) == -1) {
                         p = p.parentNode;
+                        if (p.localName == 'body') { return; }
                     }
                     p.classList.remove(activeClassName);
-                }
-                do {
-                    p = p.parentNode;
-                } while (p.localName != 'li' && !p.hasAttribute('data-title'))
+                    this.classList.add(activeClassName);
+                    break;
 
-                p.classList.add(activeClassName);
-                this.classList.remove(activeClassName);
+                case (li.hasAttribute('data-title') && li.clientHeight != 0):
+                    /* 点击标题右侧返回按钮，返回上一级 */
+                    /* TODO :参考 a 点击 */
 
-                break;
-            case ([...li.children].map(c => c.localName).indexOf('a') != -1 && li.className.indexOf(activeClassName) == -1):
-                /* 点击方块，方块点击内部超链接 */
+                    console.log("li.hasAttribute('data-title') && li.clientHeight != 0" + (li.hasAttribute('data-title') && li.clientHeight != 0));
+                    if (li.className.indexOf(activeClassName) == -1) {
+                        while (p.className.indexOf(activeClassName) == -1) {
+                            p = p.parentNode;
+                        }
+                        p.classList.remove(activeClassName);
+                    }
+                    console.log(p);
+                    do {
+                        console.log(p);
+                        p = p.parentNode;
+                        console.log(p);
+                        console.log("p.localName != 'li' && !p.hasAttribute('data-title') : " + (p.localName != 'li' && !p.hasAttribute('data-title')));
+                    } while (p.localName != 'li' && !p.hasAttribute('data-title'))
 
-                li.children[[...li.children].map(c => c.localName).indexOf('a')].click();
-                break;
+                    p.classList.add(activeClassName);
+                    this.classList.remove(activeClassName);
 
-        }
-        window.event.stopPropagation();
-        return false;
-    }, false)
-});
+                    break;
+                case ([...li.children].map(c => c.localName).indexOf('a') != -1 && li.className.indexOf(activeClassName) == -1):
+                    /* 点击方块，方块点击内部超链接 */
 
+                    console.log("([...li.children].map(c => c.localName).indexOf('a') != -1 && li.className.indexOf(activeClassName) == -1)" + ([...li.children].map(c => c.localName).indexOf('a') != -1 && li.className.indexOf(activeClassName) == -1))
+                    li.children[[...li.children].map(c => c.localName).indexOf('a')].click();
+                    break;
 
-
-
-
-
-var nextStationAttr = 'nextStation';
-function getDomStation(dom) {
-    return function () {
-        window.location.href = dom.getAttribute(nextStationAttr);
-    }
-}
-function aDelay(selector = 'a') {
-    console.log(selector);
-    [...document.querySelectorAll(selector)].map(function (a) {
-        try {
-            if (a.hasAttribute('href') && (a.href.length != 0) && (a.href.indexOf('http') != -1)) {
-                a.setAttribute(nextStationAttr, a.href)
-                a.href = 'javascript:void 0';
-                a.addEventListener('click', function clickLinkFadeOut() {
-                    document.body.classList.add('fadeOut');
-                    var dom = this;
-                    nextStation = getDomStation(dom);
-                    setTimeout('nextStation()', 750);
-                }, false);
-            };
-        } catch (err) { console.log(err); console.log(a); }
-    })
-
+            }
+            window.event.stopPropagation();
+            return false;
+        }, false)
+    });
 }
 
-aDelay();
+
+function aSetup() {
+    [...document.querySelectorAll('a[href]')].map(function (a) {
+        a.setAttribute('nextStation', a.href);
+        a.removeAttribute('href');
+        a.addEventListener('click', function clickA() {
+            console.log('clickLink');
+            document.body.classList.add('fadeOut');
+            var dom = this;
+            nextStation = function () { window.location.href = dom.getAttribute('nextStation'); };
+            setTimeout('nextStation()', 750);
+        }, false);
+    });
+}
+
+liSetup();
+aSetup();
+
 console.log('loaded script.js');
