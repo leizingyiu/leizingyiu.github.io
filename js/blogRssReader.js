c7a13e2f91ef1c9fe2bf9ebc172e87d25418884f
leizingyiu <youcanfindme@126.com>
2021-08-26 02:55:26 +0800

    add design,and fixed markdown width in mobi

--- js/blogRssReader.js
+++ js/blogRssReader.js
@@ -1,16 +1,22 @@
 
-iframe = document.createElement('iframe');
-iframe.src = window.location.origin.match('leizingyiu.net') ? location.protocol + '//blog.leizingyiu.net/rssPage/index.html' : 'http://localhost:4000/rssPage/index.html';
-iframe.id = 'rssReader';
-iframe.style.cssText = 'opacity:0;overflow:hidden;';
-iframe.frameborder = '0';
-iframe.width = iframe.height = 0;
+function initRssIframe(rssPageUrl, itemNum) {
+    var iframe = document.createElement('iframe');
+    // iframe.src = window.location.origin.match('leizingyiu.net') ? location.protocol + rssPageUrl.match(/\/\/.*/g)[0] : 'http://localhost:4000/rssPage/index.html';
+    iframe.src = location.protocol + rssPageUrl.match(/\/\/.*/g)[0];
 
-iframe.onload = function () {
-    document.getElementById(iframe.id).contentWindow.postMessage('LatestBlog:8', iframe.src.match(/.*:[\/]{2}[^\/]+/)[0]);
+    iframe.id = 'rssReader' + Math.floor(Math.random() * 100000);
+
+    iframe.style.cssText = 'opacity:0;overflow:hidden;';
+    iframe.frameborder = '0';
+    iframe.width = iframe.height = 0;
+
+    iframe.onload = function () {
+        console.log('iframe.id:  ' + iframe.id);
+        console.log('rssPageUrl' + rssPageUrl);
+        document.getElementById(iframe.id).contentWindow.postMessage('LatestBlog:' + itemNum, iframe.src.match(/.*:[\/]{2}[^\/]+/)[0]);
+    }
+    document.getElementsByTagName('body')[0].appendChild(iframe);
 }
-
-document.getElementsByTagName('body')[0].appendChild(iframe);
 
 function write_ul_li_a(contents, containerSelector) {
     var ul = document.createElement('ul');
@@ -26,9 +32,18 @@ function write_ul_li_a(contents, containerSelector) {
         li.appendChild(a);
     });
 }
+initRssIframe('http://blog.leizingyiu.net/rssPage/index.html', 8)
+initRssIframe('http://design.leizingyiu.net/rssPage/index.html', 8)
 
-write_ul_li_a([{ 'href': 'html://blog.leizingyiu.net', 'title': ' 数 据 加 载 中 请 稍 后 \n\n 点 击 前 往 博 客' }], '#blog');
+//initRssIframe('http://localhost:4000/rssPage/index.html', 8)
+//initRssIframe('http://192.168.3.171:8000/rssPage/index.html', 8)
+
+
+write_ul_li_a([{ 'href': 'html://blog.leizingyiu.net', 'title': ' 数 据 加 载 中 请 稍 候 \n\n 点 击 前 往 博 客' }], '#blog');
+write_ul_li_a([{ 'href': 'html://design.leizingyiu.net', 'title': ' 数 据 加 载 中 请 稍 候 \n\n 点 击 前 往 作 品 集' }], '#design');
+
 aSetup();
+
 window.addEventListener('message', function (e) {
     var messageObj = {};
     try {
@@ -37,6 +52,9 @@ window.addEventListener('message', function (e) {
         console.log(err);
         return;
     }
+
+    console.log("messageObj: " + JSON.stringify(messageObj, ' ', 4));
+
     Object.keys(messageObj).map(function (k) {
         write_ul_li_a(messageObj[k], '#' + k);
         liSetup();
