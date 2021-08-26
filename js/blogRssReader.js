f1f0bcc5e7c69ffe80eee2051a8323eb77c6e43c
leizingyiu <8540989+leizingyiu@users.noreply.github.com>
2021-08-26 14:11:52 +0800

    update design

--- js/blogRssReader.js
+++ js/blogRssReader.js
@@ -24,23 +24,31 @@ function write_ul_li_a(contents, containerSelector) {
     document.querySelector(containerSelector).innerHTML = '';
     document.querySelector(containerSelector).appendChild(ul);
     contents.map(function (c) {
+
         li = document.createElement('li');
         a = document.createElement('a');
-        a.href = c.href;
-        a.innerText = c.title;
+        if (c.innerText.length == 0) {
+            a.href = 'javascript:void 0;';
+            li.style.cssText = 'opacity:0!important;';
+        } else {
+            Object.keys(c).map(attr => a.setAttribute(attr, c[attr]));
+        }
+        a.innerText = a.getAttribute('innerText');
+        // a.href = c.href;
+        // a.innerText = c.innerText;
         ul.appendChild(li);
         li.appendChild(a);
     });
 }
-initRssIframe('http://blog.leizingyiu.net/rssPage/index.html', 8)
-initRssIframe('http://design.leizingyiu.net/rssPage/index.html', 8)
+//initRssIframe('http://blog.leizingyiu.net/rssPage/index.html', 8)
+//initRssIframe('http://design.leizingyiu.net/index.html', 8)
 
-//initRssIframe('http://localhost:4000/rssPage/index.html', 8)
-//initRssIframe('http://192.168.3.171:8000/rssPage/index.html', 8)
+initRssIframe('http://localhost:4000/rssPage/index.html', 8)//blog
+initRssIframe('http://192.168.0.60:8000//index.html', 8)//design
 
 
-write_ul_li_a([{ 'href': 'html://blog.leizingyiu.net', 'title': ' 数 据 加 载 中 请 稍 候 \n\n 点 击 前 往 博 客' }], '#blog');
-write_ul_li_a([{ 'href': 'html://design.leizingyiu.net', 'title': ' 数 据 加 载 中 请 稍 候 \n\n 点 击 前 往 作 品 集' }], '#design');
+write_ul_li_a([{ 'href': 'html://blog.leizingyiu.net', 'innerText': ' 数 据 加 载 中 请 稍 候 \n\n 点 击 前 往 博 客' }], '#blog');
+write_ul_li_a([{ 'href': 'html://design.leizingyiu.net', 'innerText': ' 数 据 加 载 中 请 稍 候 \n\n 点 击 前 往 作 品 集' }], '#design');
 
 aSetup();
 
@@ -59,6 +67,6 @@ window.addEventListener('message', function (e) {
         write_ul_li_a(messageObj[k], '#' + k);
         liSetup();
         aSetup();
+        dataPreviewSetup();
     });
-
 }, false);
--- js/writeDataPreview.js
+++ js/writeDataPreview.js
@@ -1,11 +1,14 @@
-[...document.querySelectorAll('a[data-preview]')].map(a => {
-    var bgImage = "--bg-image: url('" + a.getAttribute('data-preview') + "');";
-    if (a.hasAttribute('style') && a.style.cssText.indexOf("--bg-image") == -1) {
-        a.style = a.style + ';' + bgImage;
-    } else if (a.hasAttribute('style') && a.style.cssText.indexOf('--bg-image') != -1) {
+function dataPreviewSetup() {
+    [...document.querySelectorAll('a[data-preview]')].map(a => {
+        var bgImage = "--bg-image: url('" + a.getAttribute('data-preview') + "');";
+        if (a.hasAttribute('style') && a.style.cssText.indexOf("--bg-image") == -1) {
+            a.style = a.style + ';' + bgImage;
+        } else if (a.hasAttribute('style') && a.style.cssText.indexOf('--bg-image') != -1) {
 
-        a.style = a.style.cssText.replace(/(--bg-image:\surl\(['"])[^'"]*(['"]\);)/g, '$1' + a.getAttribute('data-preview') + '$2');
-    } else {
-        a.setAttribute('style', bgImage);
-    }
-})
+            a.style = a.style.cssText.replace(/(--bg-image:\surl\(['"])[^'"]*(['"]\);)/g, '$1' + a.getAttribute('data-preview') + '$2');
+        } else {
+            a.setAttribute('style', bgImage);
+        }
+    })
+}
+dataPreviewSetup();
