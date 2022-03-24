01921e3c4ac13bd00695ea178b6ba34273d47cdf
leizingyiu <youcanfindme@126.com>
2022-03-24 10:39:00 +0800

    -

---
+++ js/LoadStatisticsScript.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var script = document.createElement('script');
+    script.src = "https://s9.cnzz.com/z_stat.php?id=1280029923&web_id=1280029923";
+    script.type = "text/javascript";
+    document.body.appendChild(script);
+}
